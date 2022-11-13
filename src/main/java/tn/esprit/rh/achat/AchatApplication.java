934a543fb5b02b4968616e8557dc760849b42285
Safa Kaabi <79477715+safa-kaabi@users.noreply.github.com>
2022-11-13 14:48:26 +0100

    Update AchatApplication.java

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableSwagger2
 public class AchatApplication {
 
     public static void main(String[] args) {
