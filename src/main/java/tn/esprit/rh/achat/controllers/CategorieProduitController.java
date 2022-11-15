977ccb55b233a1d55f2630f5c1749cc6da34d382
Yesmine.BenMustapha <yesmine.benmustapha@esprit.tn>
2022-11-15 22:51:30 +0000

    Update testFile

--- src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -1,12 +1,32 @@
-package tn.esprit.rh.achat.controllers;
+package com.esprit.examen.controllers;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.entities.Produit;
+import com.esprit.examen.services.ICategorieProduitService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import tn.esprit.rh.achat.entities.CategorieProduit;
-import tn.esprit.rh.achat.services.ICategorieProduitService;
-
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 @RestController
 @Api(tags = "Gestion des categories Produit")
@@ -34,8 +54,12 @@ public class CategorieProduitController {
 	// http://localhost:8089/SpringMVC/categorieProduit/add-categorieProduit
 	@PostMapping("/add-categorieProduit")
 	@ResponseBody
-	public CategorieProduit addCategorieProduit(@RequestBody CategorieProduit cp) {
-		CategorieProduit categorieProduit = categorieProduitService.addCategorieProduit(cp);
+	public CategorieProduit addCategorieProduit(@RequestBody CategorieRequestModel  categorieRequestModel ) {
+		CategorieProduit cat = new CategorieProduit();
+		cat.setCodeCategorie(categorieRequestModel.getCodeCategorie());
+		cat.setLibelleCategorie(categorieRequestModel.getLibelleCategorie());
+		categorieProduitService.addCategorieProduit(cat);
+		CategorieProduit categorieProduit = categorieProduitService.addCategorieProduit(cat);
 		return categorieProduit;
 	}
 
@@ -49,9 +73,25 @@ public class CategorieProduitController {
 	// http://localhost:8089/SpringMVC/categorieProduit/modify-categorieProduit
 	@PutMapping("/modify-categorieProduit")
 	@ResponseBody
-	public CategorieProduit modifyCategorieProduit(@RequestBody CategorieProduit categorieProduit) {
-		return categorieProduitService.updateCategorieProduit(categorieProduit);
+	public CategorieProduit modifyCategorieProduit(@RequestBody CategorieRequestModel  categorieRequestModel) {
+		return categorieProduitService.updateCategorieProduit(new CategorieProduit(categorieRequestModel.getIdCategorieProduit(),
+				categorieRequestModel.getCodeCategorie(), categorieRequestModel.getLibelleCategorie(), categorieRequestModel.getProduits()));
 	}
 
 	
+}
+
+@Getter
+@Setter
+class CategorieRequestModel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long idCategorieProduit;
+	private String codeCategorie;
+	private String libelleCategorie;
+	@OneToMany(mappedBy = "categorieProduit")
+	@JsonIgnore
+	private Set<Produit> produits;
 }
