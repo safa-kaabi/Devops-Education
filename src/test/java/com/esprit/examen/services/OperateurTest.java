49b4e9ceb5a90e412c12fc33ee3e0cbe9f0872ce
admin <ezzina.chaima@esprit.tn>
2022-11-16 23:03:06 +0100

    test changes

--- src/test/java/com/esprit/examen/services/OperateurTest.java
+++ src/test/java/com/esprit/examen/services/OperateurTest.java
@@ -14,9 +14,15 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -32,7 +38,7 @@ public class OperateurTest{
     Operateur op = Operateur.builder().nom("aaa").prenom("bbbb").password("eeee").build();
 
     @Test
-    public void testAddOperateur() {
+    public void AddOperateurTest() {
         List<Operateur> operateurs = operateurService.retrieveAllOperateurs();
         int expected=operateurs.size();
         Operateur o = new Operateur();
@@ -44,13 +50,50 @@ public class OperateurTest{
         assertNotNull(savedOperateur.getNom());
         operateurService.deleteOperateur(savedOperateur.getIdOperateur());
     }
+    @Test
+    public void updateOperateurTest()
+    {
+    Operateur op = new Operateur();
+        op.setNom("test");
+        op.setPrenom("test2");
+        op.setPassword("test3");
+    
+        Operateur updatedOperateur=operateurService.updateOperateur(op);
+        
+    }
 
+   @Test
+   public void deleteeOperateurTest()
+   {
+        //operateurService.deleteOperateur();
+        //assertNull(operateurService.retrieveOperateur());
+   }
 
     @Test
-    public void MockAddOperateur() {
+    public void MockAddOperateurTest() {
         Mockito.when(operateurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(op));
         Operateur operateur = operateurServiceImp.retrieveOperateur((long) 1);
         assertNotNull(operateur);
         log.info("get operateur"+operateur);
     }
+
+    @Test
+    public void saveOperateurTest() {
+		when(operateurRepository.findAll()).thenReturn(Stream
+				.of(new Operateur((long)12,"mock","mocks","123000",null),new Operateur((long)15,"mock1","mocks1","123001",null)).collect(Collectors.toList()));
+				
+	}
+    @Test
+    public void UpdateOperateurTestMockito() {
+        when(operateurRepository.save(op)).thenReturn(op);
+        assertNotNull(op);
+        assertEquals(op, operateurService.updateOperateur(op));
+    }
+	@Test
+	public void deleteOperateurTest() {
+		Operateur operateur = new Operateur((long)12,"mock","mocks","123000",null);
+		//operateurService.deleteOperateur(operateur.getIdOperateur());
+		//verify(operateurRepository, times(1)).delete(operateur);
+	}	
+
 }
