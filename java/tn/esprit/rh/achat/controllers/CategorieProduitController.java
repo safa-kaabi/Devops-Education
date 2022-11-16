e4e2f97a50e32407b19750a81f0f8976addc5282
Yesmine.BenMustapha <yesmine.benmustapha@esprit.tn>
2022-11-16 00:56:20 +0000

    Update test

--- java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -1,32 +1,20 @@
-package com.esprit.examen.controllers;
-
-import java.io.Serializable;
-import java.util.List;
-import java.util.Set;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import com.esprit.examen.entities.CategorieProduit;
-import com.esprit.examen.entities.Produit;
-import com.esprit.examen.services.ICategorieProduitService;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+package tn.esprit.rh.achat.controllers;
 
 import io.swagger.annotations.Api;
 import lombok.Getter;
 import lombok.Setter;
+import tn.esprit.rh.achat.entities.CategorieProduit;
+import tn.esprit.rh.achat.services.ICategorieProduitService;
+import tn.esprit.rh.achat.entities.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.OneToMany;
 
 @RestController
 @Api(tags = "Gestion des categories Produit")
