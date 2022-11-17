f23f63ffe062f33da271108937eca2176f813af6
NegraMed <52628123+NegraMed@users.noreply.github.com>
2022-11-17 10:49:14 +0100

    Update OperateurTest.java

--- src/test/java/com/esprit/examen/services/OperateurTest.java
+++ src/test/java/com/esprit/examen/services/OperateurTest.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+/*package com.esprit.examen.services;
 
 import com.esprit.examen.entities.Operateur;
 import com.esprit.examen.repositories.OperateurRepository;
@@ -53,4 +53,4 @@ public class OperateurTest{
         assertNotNull(operateur);
         log.info("get operateur"+operateur);
     }
-}
+}*/
